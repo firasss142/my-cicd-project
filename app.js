ddc04ad489308f8e2f014346be040a5fdc779c93
fiRas-kaRchoud <steamneedyup@gmail.com>
2025-12-18 13:26:55 +0100

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello from CI/CD Pipeline  🚀');  // Changed this line
+  res.send('bou lalla');  // Changed this line
 });
 
 app.listen(port, () => {
